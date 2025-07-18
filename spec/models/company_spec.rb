3f3f49013fc8cd6d67693a0bee75596acd140e04
Vitor Navarro <vitor.navarro@v360.io>
2025-07-18 18:25:35 -0300

    correct company name on test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   it 'is valid with a name' do
-    expect(Company.new(name: 'ACME')).to be_valid
+    expect(Company.new(name: 'ABCorp')).to be_valid
   end
 
   it 'is invalid without a name' do
