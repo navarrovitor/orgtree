05636a8f1cc2bb087c3f155468cb7a7b1768a83c
Vitor Navarro <vitor.navarro@v360.io>
2025-07-21 13:40:30 -0300

    remove useless comments

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,7 +1,5 @@
 class CompanySerializer < ActiveModel::Serializer
   attributes :id, :name
 
-  # Quando um único registro de Company for serializado,
-  # esta linha incluirá um array de seus funcionários.
   has_many :employees
 end
--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -2,7 +2,6 @@ class EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :picture
 
   # Para as associações, usamos o serializer básico para evitar loops.
-  # Na requisição de empregado, ele virá com dados completos. Os dados do gestor e subordinados, virão com menos dados (id, name, email)
   belongs_to :manager, serializer: BasicEmployeeSerializer
   has_many :subordinates, serializer: BasicEmployeeSerializer
 end
