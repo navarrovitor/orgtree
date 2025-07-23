179a3cff7f3febfb4f301e4a6540bdfe5ed3f296
Vitor Navarro <vitor.navarro@v360.io>
2025-07-23 09:42:24 -0300

    alter employee controller to accept manager deassigning

--- api/app/controllers/api/v1/employees_controller.rb
+++ api/app/controllers/api/v1/employees_controller.rb
@@ -1,18 +1,14 @@
 class Api::V1::EmployeesController < Api::V1::ApplicationController
   before_action :set_employee, only: [:destroy, :peers, :subordinates, :second_level_subordinates, :assign_manager]
 
-  # DELETE /api/v1/employees/:id
   def destroy
     @employee.destroy
-    head :no_content # Retorna uma resposta 204 No Content
+    head :no_content
   end
 
-  # POST /api/v1/employees/:id/assign_manager
-  # Corpo da requisição esperado: { "manager_id": <id_do_novo_gestor> }
-  # A User Story 3.1 pede para "associar um colaborador como gestor de outro usuário"
   def assign_manager
-    manager_id = params.require(:manager_id)
-    manager = Employee.find(manager_id)
+    manager_id = params[:manager_id]
+    manager = manager_id.present? ? Employee.find(manager_id) : nil
 
     @employee.manager = manager
 
@@ -23,11 +19,8 @@ class Api::V1::EmployeesController < Api::V1::ApplicationController
     end
   end
 
-  # GET /api/v1/employees/:id/peers
   def peers
-    # A User Story 3.2 pede para "listar os pares de um colaborador"
     if @employee.manager.present?
-      # Tira o próprio funcionário da lista
       peers = @employee.manager.subordinates.where.not(id: @employee.id)
       render json: peers
     else
@@ -35,15 +28,11 @@ class Api::V1::EmployeesController < Api::V1::ApplicationController
     end
   end
 
-  # GET /api/v1/employees/:id/subordinates
   def subordinates
-    # A User Story 3.3 pede para "listar os liderados diretos de um colaborador"
     render json: @employee.subordinates
   end
 
-  # GET /api/v1/employees/:id/second_level_subordinates
   def second_level_subordinates
-    # A User Story 3.4 pede para "listar os liderados dos liderados de um colaborador"
     render json: @employee.second_level_subordinates
   end
 
